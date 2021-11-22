fd07b7980384964db23f4f8c48c3394f3db62420
PatilOjas <ojaspatil1901@gmail.com>
2021-11-22 16:57:44 +0530

    Server Updated

--- paigham/lib/screens/chatDetailPage.dart
+++ paigham/lib/screens/chatDetailPage.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:paigham/models/chatMessageModel.dart';
+import 'dart:io';
 
-class ChatDetailPage extends StatefulWidget{
+class ChatDetailPage extends StatefulWidget {
   @override
   _ChatDetailPageState createState() => _ChatDetailPageState();
 }
 
 class _ChatDetailPageState extends State<ChatDetailPage> {
+  TextEditingController message = TextEditingController();
 
   List<ChatMessage> messages = [
     ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
     ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
-    ChatMessage(messageContent: "Hey Kriss, I am doing fine dude. wbu?", messageType: "sender"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
     ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
-    ChatMessage(messageContent: "Is there any thing wrong?", messageType: "sender"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
   ];
 
   @override
@@ -29,29 +34,108 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
             child: Row(
               children: <Widget>[
                 IconButton(
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.pop(context);
                   },
-                  icon: Icon(Icons.arrow_back,color: Colors.black,),
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
                 ),
-                SizedBox(width: 2,),
+                SizedBox(
+                  width: 2,
+                ),
                 CircleAvatar(
-                  backgroundImage: NetworkImage("<https://randomuser.me/api/portraits/men/5.jpg>"),
+                  backgroundImage: NetworkImage(
+                      "<https://randomuser.me/api/portraits/men/5.jpg>"),
                   maxRadius: 20,
                 ),
-                SizedBox(width: 12,),
+                SizedBox(
+                  width: 12,
+                ),
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Text("Kriss Benwat",style: TextStyle( fontSize: 16 ,fontWeight: FontWeight.w600),),
-                      SizedBox(height: 6,),
-                      Text("Online",style: TextStyle(color: Colors.grey.shade600, fontSize: 13),),
+                      Text(
+                        "Kriss Benwat",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                      ),
                     ],
                   ),
                 ),
-                Icon(Icons.settings,color: Colors.black54,),
+                SizedBox(
+                  width: 15,
+                ),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  child: FittedBox(
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.white,
+                      onPressed: () {
+                        print("print Dictionaries");
+                      },
+                      child: Icon(
+                        Icons.call_rounded,
+                        color: Colors.blue[800],
+                        size: 30,
+                      ),
+                      elevation: 0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  child: FittedBox(
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.white,
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.video_call_rounded,
+                        color: Colors.blue[800],
+                        size: 32,
+                      ),
+                      //backgroundColor: Colors.blue,
+                      elevation: 0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  child: FittedBox(
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.white,
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.receipt_long_rounded,
+                        color: Colors.blue[800],
+                        size: 30,
+                      ),
+                      //backgroundColor: Colors.blue,
+                      elevation: 0,
+                    ),
+                  ),
+                ),
+                //Icon(Icons.settings,color: Colors.black54,),
               ],
             ),
           ),
@@ -62,20 +146,28 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
           ListView.builder(
             itemCount: messages.length,
             shrinkWrap: true,
-            padding: EdgeInsets.only(top: 10,bottom: 10),
+            padding: EdgeInsets.only(top: 10, bottom: 10),
             physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index){
+            itemBuilder: (context, index) {
               return Container(
-                padding: EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
+                padding:
+                    EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
                 child: Align(
-                  alignment: (messages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
-                      color: (messages[index].messageType  == "receiver"?Colors.grey.shade200:Colors.blue[200]),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.grey.shade200
+                          : Colors.blue[200]),
                     ),
                     padding: EdgeInsets.all(16),
-                    child: Text(messages[index].messageContent, style: TextStyle(fontSize: 15),),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
                 ),
               );
@@ -84,15 +176,14 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
           Align(
             alignment: Alignment.bottomLeft,
             child: Container(
-              padding: EdgeInsets.only(left: 10,bottom: 10,top: 10),
+              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
               height: 60,
               width: double.infinity,
               color: Colors.white,
               child: Row(
                 children: <Widget>[
                   GestureDetector(
-                    onTap: (){
-                    },
+                    onTap: () {},
                     child: Container(
                       height: 30,
                       width: 30,
@@ -100,28 +191,41 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
                         color: Colors.lightBlue,
                         borderRadius: BorderRadius.circular(30),
                       ),
-                      child: Icon(Icons.add, color: Colors.white, size: 20, ),
-                    ),
-                  ),
-                  SizedBox(width: 15,),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "Write message...",
-                        hintStyle: TextStyle(color: Colors.black54),
-                        border: InputBorder.none
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 20,
                       ),
                     ),
                   ),
-                  SizedBox(width: 15,),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: message,
+                      decoration: InputDecoration(
+                          hintText: "Write message...",
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
                   FloatingActionButton(
-                    onPressed: (){},
-                    child: Icon(Icons.send,color: Colors.white,size: 18,),
+                    onPressed: () {
+                      print(message);
+                    },
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
                     backgroundColor: Colors.blue,
                     elevation: 0,
                   ),
                 ],
-                
               ),
             ),
           ),
